d1ed883fddeacae2ae0d0b785303231e18bb47b4
fengcenio <github@fengcen.io>
2025-08-21 07:30:53 +0800

    feat: ✨ add singleton window shortcut and update Sliprail SDK

--- extensions/window/main.mjs
+++ extensions/window/main.mjs
@@ -9,10 +9,43 @@ export default {
       handle: (context) => {
         const channel = sliprailMain.createWindowChannel();
         channel.on('getMessage', () => {
-          channel.emit('message', 'Hello from Extension!');
+          channel.emit('message', 'Hello from Window Shortcut!');
         });
 
-        context.showWindow({
+        context.createWindow({
+          channel,
+          htmlFile: 'index.html',
+          title: 'Window Example',
+          width: 600,
+          height: 500,
+          maximize: false,
+          parameters: {
+            initialParameter: 'test',
+          },
+          resizable: true,
+          onClose: () => {},
+        });
+      },
+    },
+    {
+      id: 'ShortcutExampleWindow',
+      displayName: 'Shortcut Example - Singleton Window',
+      /**
+       * @param context {import('@sliprail/sdk').ShortcutContext}
+       */
+      handle: (context) => {
+        const singletonWindow = context.getWindows()[0];
+        if (singletonWindow != null) {
+          singletonWindow.show();
+          return;
+        }
+
+        const channel = sliprailMain.createWindowChannel();
+        channel.on('getMessage', () => {
+          channel.emit('message', 'Hello from Singleton Window Shortcut!');
+        });
+
+        context.createWindow({
           channel,
           htmlFile: 'index.html',
           title: 'Window Example',
